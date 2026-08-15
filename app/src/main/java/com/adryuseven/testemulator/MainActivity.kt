ae9719d352fae600b35e06fdec7f67185b483409
AdryU7 <adri_francis2003@outlook.com>
2026-08-14 21:00:37 -0500

    Image

--- app/src/main/java/com/adryuseven/testemulator/MainActivity.kt
+++ app/src/main/java/com/adryuseven/testemulator/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
@@ -28,8 +30,11 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -76,6 +81,7 @@ fun MyButton() {
 
         MyOutlinedButton()
         MyTextButton()
+        MyImage()
     }
 }
 
@@ -149,3 +155,16 @@ fun Greetings(name: String) {
 fun GreetingsPreview(){
     Greetings(name = "Jorge")
 }*/
+
+@Composable
+fun MyImage() {
+    Image(
+        painter = painterResource(id = R.drawable.mi_imagen),
+        contentDescription = "Descripcion de la imagen",
+        modifier = Modifier
+            .size(150.dp)
+            .clip(CircleShape)
+            .border(2.dp, Color.Yellow, CircleShape),
+        contentScale = ContentScale.Crop
+    )
+}
