5216e672e6cceb471d6cf8968d02adfe59f5461c
AdryU7 <adri_francis2003@outlook.com>
2026-08-14 20:50:49 -0500

    Button - OutlinedButton - TextButton

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,8 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
+    // Dependencia obligatoria para tener acceso a Icons
+    implementation("androidx.compose.material:material-icons-core:1.6.11")
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
