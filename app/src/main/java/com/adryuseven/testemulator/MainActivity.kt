e5522dfa1b5e25c65dc067c79177cfb9b23ad164
AdryU7 <adri_francis2003@outlook.com>
2026-09-13 14:26:47 -0500

    Layouts: Column, Row, Box, ConstraintLayout

--- app/src/main/java/com/adryuseven/testemulator/MainActivity.kt
+++ app/src/main/java/com/adryuseven/testemulator/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -50,6 +52,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.adryuseven.testemulator.ui.theme.TestEmulatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -66,27 +70,142 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Components() {
-    Column(
+    MyConstraintLayout() // Layout
+}
+
+// ==== LAYOUTS ====
+@Composable
+fun MyColumn() {
+    Column (
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .padding(15.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
     ) {
-        val backgroundColor = MaterialTheme.colorScheme.primary
+        MyOutlinedButton()
+        MyTextButton()
+        MyImage()
+        MyIcon()
+    }
+}
 
-        Canvas(
-            modifier = Modifier
-                .size(300.dp)
-                .padding(15.dp)
-        ) {
-            drawCircle(
-                color = backgroundColor,
-                radius = size.minDimension/4 // radius = 75
+@Composable
+fun MyRow() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+            .background(MaterialTheme.colorScheme.primaryContainer)
+            .padding(15.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = "Elemento 1")
+        Text(text = "Elemento 2")
+    }
+}
+
+@Composable
+fun MyBox() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.imagen_fondo),
+            contentDescription = "Imagen de fondo",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        Text(
+            text = "Curso de Jetpack Compose",
+            style = TextStyle(
+                color = Color.Black,
+                fontSize = 25.sp
             )
+        )
+
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(50.dp),
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary
+            )
+        ) {
+            Text(text = "Presione")
         }
     }
 }
+
+@Composable
+fun MyConstraintLayout() {
+    ConstraintLayout(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+    ) {
+        // Referencias para utilizar las restricciones
+        val (image, name, description, button) = createRefs()
+
+        Image (
+            painter = painterResource(id = R.drawable.perfil),
+            contentDescription = "Imagen de perfil",
+            modifier = Modifier
+                .size(100.dp)
+                // Aplicar restricciones con el elemento parent para posicionar la imagen
+                .constrainAs(image){
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                },
+            contentScale = ContentScale.Crop
+        )
+
+        Text(
+            text = "Cristiano Ronaldo",
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier
+                // Aplicar restricciones con el elemento image para posicionar el nombre
+                .constrainAs(name){
+                    top.linkTo(image.top)
+                    start.linkTo(image.end, margin = 16.dp) // Dar margen para dar distancia al contenido
+                }
+        )
+
+        Text(
+            text = "Jugador de Portugal",
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier
+                .constrainAs(description){
+                    // Posicionar la descripcion con el elemento name y un margen
+                    top.linkTo(name.bottom, margin = 4.dp)
+                    start.linkTo(name.start)
+                    // Alinear con el borde derecho del contenedor
+                    end.linkTo(parent.end)
+                    // Ocupar t0do el ancho de la parte izquierda y derecha del texto
+                    width = Dimension.fillToConstraints
+                }
+        )
+
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .constrainAs(button){
+                    top.linkTo(description.bottom, margin = 16.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        ) {
+            Text(text = "Seguir")
+        }
+    }
+}
+// ==== XXXXX ====
 
 /* == Previous Components ==
     Button(
@@ -138,6 +257,27 @@ fun Components() {
             modifier = Modifier.fillMaxWidth(),
             color = MaterialTheme.colorScheme.primary
         )
+   == CANVAS ==
+   Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val backgroundColor = MaterialTheme.colorScheme.primary
+
+        Canvas(
+            modifier = Modifier
+                .size(300.dp)
+                .padding(15.dp)
+        ) {
+            drawCircle(
+                color = backgroundColor,
+                radius = size.minDimension/4 // radius = 75
+            )
+        }
+    }
 */
 
 @Composable
